e2300d9a8bdccf4b339eff4b16e82ee9376aaa8d
Souryo <mesenemu@gmail.com>
2016-05-22 19:30:17 -0400

    Cleanup: Removed unused variable

--- GUI.NET/Config/PlayerProfile.cs
+++ GUI.NET/Config/PlayerProfile.cs
@@ -12,29 +12,5 @@ namespace Mesen.GUI.Config
 	public class PlayerProfile
 	{
 		public string PlayerName = "NewPlayer";
-
-		[NonSerialized]
-		public byte[] PlayerAvatar;
-
-		public PlayerProfile()
-		{
-			//SetAvatar(Properties.Resources.MesenLogo);
-		}
-		/*
-
-		public void SetAvatar(Image image)
-		{
-			PlayerAvatar = image.ResizeImage(64, 64).ToByteArray(ImageFormat.Bmp);
-		}
-
-		public void SetAvatar(string filename)
-		{
-			PlayerAvatar = File.ReadAllBytes(filename);
-		}
-
-		public Image GetAvatarImage()
-		{
-			return Image.FromStream(new MemoryStream(PlayerAvatar));
-		}*/
 	}
 }
