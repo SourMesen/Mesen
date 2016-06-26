7687eccd70cb392368795754f36ba977cd8c859e
Souryo <mesenemu@gmail.com>
2016-06-25 22:06:22 -0400

    Debugger: Fixed crash that occured when reloading saved breakpoints

--- GUI.NET/Debugger/frmDebugger.cs
+++ GUI.NET/Debugger/frmDebugger.cs
@@ -23,43 +23,41 @@ namespace Mesen.GUI.Debugger
 		public frmDebugger()
 		{
 			InitializeComponent();
-
-			if(!this.DesignMode) {
-				this.mnuSplitView.Checked = ConfigManager.Config.DebugInfo.SplitView;
-				this.mnuPpuPartialDraw.Checked = ConfigManager.Config.DebugInfo.PpuPartialDraw;
-
-				_lastCodeWindow = ctrlDebuggerCode;
-
-				this.ctrlDebuggerCode.SetConfig(ConfigManager.Config.DebugInfo.LeftView);
-				this.ctrlDebuggerCodeSplit.SetConfig(ConfigManager.Config.DebugInfo.RightView);
-
-				BreakpointManager.Breakpoints.Clear();
-				BreakpointManager.Breakpoints.AddRange(ConfigManager.Config.DebugInfo.Breakpoints);
-				BreakpointManager.BreakpointsChanged += BreakpointManager_BreakpointsChanged;
-				this.ctrlBreakpoints.RefreshList();
-				RefreshBreakpoints();
-
-				this.toolTip.SetToolTip(this.picWatchHelp, 
-					"Most expressions/operators are accepted (C++ syntax)." + Environment.NewLine + 
-					"Note: Use the $ prefix to denote hexadecimal values." + Environment.NewLine + Environment.NewLine + 
-					"A/X/Y/PS/SP: Value of registers" + Environment.NewLine +
-					"Irq/Nmi: True if the Irq/Nmi flags are set" + Environment.NewLine + 
-					"Cycle/Scanline: Current cycle (0-340)/scanline(-1 to 260) of the PPU" + Environment.NewLine + 
-					"Value: Current value being read/written from/to memory" + Environment.NewLine +
-					"[<address>]: Value at address (CPU)" + Environment.NewLine + Environment.NewLine + 
-					
-					"Examples:" + Environment.NewLine + 
-					"a == 10 || x == $23" + Environment.NewLine + 
-					"scanline == 10 && (cycle >= 55 && cycle <= 100)" + Environment.NewLine + 
-					"x == [$150] || y == [10]" + Environment.NewLine + 
-					"[[$15] + y]   -> Reads the value at address $15, adds Y to it and reads the value at the resulting address."
-				);
-			}
 		}
 
 		protected override void OnLoad(EventArgs e)
 		{
  			base.OnLoad(e);
+
+			this.mnuSplitView.Checked = ConfigManager.Config.DebugInfo.SplitView;
+			this.mnuPpuPartialDraw.Checked = ConfigManager.Config.DebugInfo.PpuPartialDraw;
+
+			_lastCodeWindow = ctrlDebuggerCode;
+
+			this.ctrlDebuggerCode.SetConfig(ConfigManager.Config.DebugInfo.LeftView);
+			this.ctrlDebuggerCodeSplit.SetConfig(ConfigManager.Config.DebugInfo.RightView);
+
+			BreakpointManager.Breakpoints.Clear();
+			BreakpointManager.Breakpoints.AddRange(ConfigManager.Config.DebugInfo.Breakpoints);
+			BreakpointManager.BreakpointsChanged += BreakpointManager_BreakpointsChanged;
+			this.ctrlBreakpoints.RefreshList();
+			RefreshBreakpoints();
+
+			this.toolTip.SetToolTip(this.picWatchHelp,
+				"Most expressions/operators are accepted (C++ syntax)." + Environment.NewLine +
+				"Note: Use the $ prefix to denote hexadecimal values." + Environment.NewLine + Environment.NewLine +
+				"A/X/Y/PS/SP: Value of registers" + Environment.NewLine +
+				"Irq/Nmi: True if the Irq/Nmi flags are set" + Environment.NewLine +
+				"Cycle/Scanline: Current cycle (0-340)/scanline(-1 to 260) of the PPU" + Environment.NewLine +
+				"Value: Current value being read/written from/to memory" + Environment.NewLine +
+				"[<address>]: Value at address (CPU)" + Environment.NewLine + Environment.NewLine +
+
+				"Examples:" + Environment.NewLine +
+				"a == 10 || x == $23" + Environment.NewLine +
+				"scanline == 10 && (cycle >= 55 && cycle <= 100)" + Environment.NewLine +
+				"x == [$150] || y == [10]" + Environment.NewLine +
+				"[[$15] + y]   -> Reads the value at address $15, adds Y to it and reads the value at the resulting address."
+			);
 
 			_notifListener = new InteropEmu.NotificationListener();
 			_notifListener.OnNotification += _notifListener_OnNotification;
