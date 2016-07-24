4bcb12fd3152f1651f7d789c5e41bee16d8b16ab
Souryo <mesenemu@gmail.com>
2016-07-24 14:38:02 -0400

    Version Update: 0.4.0

--- GUI.NET/Forms/frmAbout.Designer.cs
+++ GUI.NET/Forms/frmAbout.Designer.cs
@@ -143,7 +143,7 @@
 			this.labelVersion.Name = "labelVersion";
 			this.labelVersion.Size = new System.Drawing.Size(146, 17);
 			this.labelVersion.TabIndex = 0;
-			this.labelVersion.Text = "Version: 0.3.2 (Beta)";
+			this.labelVersion.Text = "Version: 0.4.0 (Beta)";
 			this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// okButton
