b6163da03d8a9b34d7191f70d0d2a817c1d6a773
Souryo <mesenemu@gmail.com>
2016-12-24 16:08:40 -0500

    Linux: Minor fix when checking why core library could not be loaded

--- GUI.NET/RuntimeChecker.cs
+++ GUI.NET/RuntimeChecker.cs
@@ -20,8 +20,15 @@ namespace Mesen.GUI
 				return InteropEmu.TestDll();
 			} catch {
 			}
+			
+			bool dllExists;
+			if(Program.IsMono) {
+				dllExists = File.Exists(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "libMesenCore.dll"));
+			} else {
+				dllExists = File.Exists("MesenCore.dll");
+			}
 
-			if(!File.Exists("MesenCore.dll") && !File.Exists("libMesenCore.dll")) {
+			if(!dllExists) {
 				MesenMsgBox.Show("UnableToStartMissingFiles", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			} else {
 				MesenMsgBox.Show("UnableToStartMissingDependencies", MessageBoxButtons.OK, MessageBoxIcon.Error);
