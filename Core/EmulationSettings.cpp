49c29693bf665e1ef50ca0e8fc6631d34370d34c
Souryo <mesenemu@gmail.com>
2017-04-15 16:58:23 -0400

    Version Update: 0.8.1

--- Core/EmulationSettings.cpp
+++ Core/EmulationSettings.cpp
@@ -3,10 +3,10 @@
 #include "Console.h"
 #include "VsControlManager.h"
 
-//Version 0.8.0
+//Version 0.8.1
 uint16_t EmulationSettings::_versionMajor = 0;
 uint8_t EmulationSettings::_versionMinor = 8;
-uint8_t EmulationSettings::_versionRevision = 0;
+uint8_t EmulationSettings::_versionRevision = 1;
 
 Language EmulationSettings::_displayLanguage = Language::English;
 
