23f96df5b756e19b5a39b8b478308e27e89b8431
Sour <mesenemu@gmail.com>
2018-06-05 18:52:28 -0400

    Debugger: Warn the user when a non-monospace font is selected

--- GUI.NET/Debugger/FontDialogHelper.cs
+++ GUI.NET/Debugger/FontDialogHelper.cs
@@ -19,7 +19,20 @@ namespace Mesen.GUI.Debugger
 				fd.ShowEffects = false;
 				fd.ShowHelp = false;
 				if(fd.ShowDialog() == DialogResult.OK) {
-					return fd.Font;
+					Font font = fd.Font;
+
+					Size sizeM = TextRenderer.MeasureText("M", font);
+					Size sizeDot = TextRenderer.MeasureText(".", font);
+
+					if(sizeM != sizeDot) {
+						if(MessageBox.Show("The font you've selected (" + fd.Font.FontFamily.Name.ToString() + ") doesn't appear to be a monospace font. Using anything other than a monospace font will cause display issues in the UI. Are you sure you want to use this font?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
+							return fd.Font;
+						} else {
+							return currentFont;
+						}
+					} else {
+						return fd.Font;
+					}
 				} else {
 					return currentFont;
 				}
