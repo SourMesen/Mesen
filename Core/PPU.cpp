36b88ac6afa7e1cdb5abdf257d83b948e4010892
Sour <mesenemu@gmail.com>
2018-07-06 19:57:51 -0400

    Debugger: Don't auto-start debugger when oam decay is turned on

--- Core/PPU.cpp
+++ Core/PPU.cpp
@@ -1025,7 +1025,7 @@ uint8_t PPU::ReadSpriteRam(uint8_t addr)
 			return _spriteRAM[addr];
 		} else {
 			//If this 8-byte row hasn't been read/written to in over 3000 cpu cycles (~1.7ms), return 0xFF to simulate decay
-			shared_ptr<Debugger> debugger = _console->GetDebugger();
+			shared_ptr<Debugger> debugger = _console->GetDebugger(false);
 			if(debugger && debugger->CheckFlag(DebuggerFlags::BreakOnDecayedOamRead)) {
 				debugger->BreakImmediately();
 			}
