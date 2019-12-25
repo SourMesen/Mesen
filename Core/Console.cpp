13e358a47c119632ee2d63b36036c66a4186f6bc
Sour <mesenemu@gmail.com>
2019-12-24 22:31:26 -0500

    Run Ahead: Turn off runahead when fast forwarding

--- Core/Console.cpp
+++ Core/Console.cpp
@@ -764,7 +764,7 @@ void Console::Run()
 	try {
 		while(true) {
 			stringstream runAheadState;
-			bool useRunAhead = _settings->GetRunAheadFrames() > 0 && !_debugger && !_rewindManager->IsRewinding();
+			bool useRunAhead = _settings->GetRunAheadFrames() > 0 && !_debugger && !_rewindManager->IsRewinding() && _settings->GetEmulationSpeed() > 0 && _settings->GetEmulationSpeed() <= 100;
 			if(useRunAhead) {
 				RunFrameWithRunAhead(runAheadState);
 			} else {
