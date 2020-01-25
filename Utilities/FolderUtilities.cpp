ffe2c480cb5942d580bf1f83eb506d8b048beed9
Sour <mesenemu@gmail.com>
2020-01-24 22:29:23 -0500

    Build: Allow compilation on older compilers by detecting filesystem API location

--- Utilities/FolderUtilities.cpp
+++ Utilities/FolderUtilities.cpp
@@ -1,8 +1,13 @@
 #include "stdafx.h"
 
 #ifndef LIBRETRO
+#if __has_include(<filesystem>)
 	#include <filesystem>
 	namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem;
+#endif
 #endif
 
 #include <unordered_set>
