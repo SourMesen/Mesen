8fa506631a7285523819c9fae44fe2d4bc983d2f
norill <norill@wp.pl>
2020-04-10 23:44:45 +0200

    Mapper 15: Fixed PRG mask for mode 1 (#803)

--- Core/Mapper15.h
+++ Core/Mapper15.h
@@ -41,7 +41,7 @@ protected:
 				bank |= subBank;
 				SelectPRGPage(0, bank);
 				SelectPRGPage(1, bank + 1);
-				bank = ((mode == 3) ? bank : (bank | 0x14)) | subBank;
+				bank = ((mode == 3) ? bank : (bank | 0x0E)) | subBank;
 				SelectPRGPage(2, bank + 0);
 				SelectPRGPage(3, bank + 1);
 				break;
